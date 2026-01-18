3be6b32c9757bf3aa4202aa1ac105c0e013fbdc9
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-01-18 19:34:05 +0530

    Add files via upload

---
+++ stdin1.java
@@ -0,0 +1,19 @@
+ import java.util.*;
+
+public class stdin1 {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+int myInt = scanner.nextInt();
+int myInt1 = scanner.nextInt();
+int myInt2 = scanner.nextInt();
+scanner.close();
+
+System.out.println(myInt);
+System.out.println( myInt1);
+System.out.println( myInt2);
+    }
+}
+ 
+    
+
