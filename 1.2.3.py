a1180f7c1efdbd30e1569d9d05d08cc6a462524b
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-08-16 21:14:42 +0530

    Add files via upload

---
+++ 1.2.3.py
@@ -0,0 +1,10 @@
+radius = float(input("Enter radius: "))
+print("Area of Circle =", 3.14 * radius * radius)
+
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+print("Area of Triangle =", 0.5 * base * height)
+
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+print("Area of Rectangle =", length * breadth)
