62abde2e4595184b63e90b3d672684bcdcd414d2
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-08-18 22:38:47 +0530

    Add files via upload

---
+++ 2.3.3.py
@@ -0,0 +1,17 @@
+print("Demonstrating break statement")
+for i in range(1, 6):
+    if i == 3:
+        break
+    print(i)
+
+print("\nDemonstrating continue statement")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+
+print("\nDemonstrating pass statement")
+for i in range(1, 6):
+    if i == 3:
+        pass
+    print(i)
