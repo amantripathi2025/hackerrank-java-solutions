ac0a776dfffb4462bb009acf745c45d1e2ffe634
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-08-20 22:21:02 +0530

    Add files via upload

---
+++ 3.1.2.py
@@ -0,0 +1,32 @@
+def greet():
+    print("Hello, Welcome to Python!")
+
+greet()
+
+
+def add(a, b):
+    print("Addition:", a + b)
+
+add(10, 20)
+
+
+def multiply(a, b):
+    return a * b
+
+result = multiply(5, 4)
+print("Multiplication:", result)
+
+
+def divide(a,b) :
+    return a/b
+result1 = divide(50,25)
+print("Divide: ",result1)
+
+def Divide(a,b):
+    print("After division: ",a/b)
+    
+Divide(1000000,250)
+
+
+def
+
