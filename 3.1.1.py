e244ba56370b4aa0dd1958160d3044bc5021f883
Aman Pati Tripathi <25LBCS1236@culkomail.in>
2026-08-23 22:32:59 +0530

    Add files via upload

---
+++ 3.1.1.py
@@ -0,0 +1,25 @@
+#len()
+numbers = [10,30,20,40,60,50,70]
+name = ['Aman','Agrav','Gyanendra']
+print("Length: ",len(numbers))
+print("Length: ",len(name))
+
+#abs()
+num = -26
+print("Absolute value: ",abs(num))
+
+#max()
+print("Maximum: ",max(numbers))
+
+#min()
+print("Minimum: ",min(numbers))
+
+#sum()
+print("Sum is: ",sum(numbers))
+
+#sorted()
+print("After sorting: ", sorted(numbers))
+
+#round()
+num1 = 13.4567
+print("Round: ",round(num1,2))
